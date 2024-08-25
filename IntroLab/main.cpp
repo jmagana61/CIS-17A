16c59081455d6309fd5ff7b07d14ac1595b788a8
jmagana61 <164950338+jmagana61@users.noreply.github.com>
2024-08-25 14:07:56 -0700

    Add files via upload

---
+++ IntroLab/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Julian Magana
+ * Created on August 25, 2024, 2:00 PM
+ * *Purpose: Assignment for intro lab
+ */
+
+#include <iostream>
+using namespace std;
+
+//System Libraries
+
+//User Libraries
+
+//Global Constants not Variables
+//Math/Physics/Science/Conversions/Dimensions
+
+//Function Prototype
+
+//Code Begins Execution here with function main
+int main(int argc, char** argv) {
+    //Set random number seed once here
+    
+    //Declare variables here
+    
+    //Initialize Variables Here
+
+    //Map inputs to outputs here, i.e the process
+    
+    //Display the results
+    cout << "Hello World!" << endl ;
+    
+    return 0;
+}
+
